a0db67f6a3a2158531a57dd41c5da9cb6205800f
Nabeel Khan <nabeel.kay@gmail.com>
2023-06-04 16:20:31 +0100

    wip

--- withdraw-deposit.js
+++ withdraw-deposit.js
@@ -4,33 +4,14 @@ let withdrawAmount = document.querySelector('#withdraw-amount');
 let depositAmount = document.querySelector('#deposit-amount');
 let currentBalance = document.querySelector('#current-balance');
 let currentDeposit = document.querySelector('#current-deposit');
-let currentWithdraw = document.querySelector('#current-withdraw')
+let currentWithdraw = document.querySelector('#current-withdraw');
 
-
-// depositBtn.addEventListener('click', function(){
-// let totalBalance= depositAmount+currentBalanceValue
-// console.log(totalBalance);})
-
-// currentBalance.innerHTML=`${currentBalanceValue}`
-
-// const updateBalance = depositAmount.addEventListener('input', function () {
-// 	let value=depositAmount.value
-//   let valueAsNumber=parseFloat(depositAmount.value)
-//   currentBalance.innerHTML=valueAsNumber
-// });
-
- depositBtn.addEventListener('click', function () {
+depositBtn.addEventListener('click', function () {
   let value = depositAmount.value;
   let valueAsNumber = parseFloat(depositAmount.value);
   currentBalance.innerHTML = valueAsNumber;
-  
-
-  });
-
-  
-
-
-
+  depositAmount.value = '';
+});
 
 depositBtn.addEventListener('click', function () {
   let value = depositAmount.value;
